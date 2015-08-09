c3c74326880e583143c5fcd1d479f016bec6b827
shprink <julien.renaux@gmail.com>
2015-08-09 17:08:48 -0500

    first batch

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ios-android-wordpress-ionic-webpack-ES6
 Repo created for the TutsPlus article: Creating iOS/Android mobile applications for WordPress using Ionic SDK, Webpack, ES6 and WP-API
+
+## Install
+
+```
+cp config/default.config.json config/config.json
+npm install
+```
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+var path = require('path'),
+    libPath = path.join(__dirname, 'lib'),
+    wwwPath = path.join(__dirname, 'www'),
+    pkg = require('./package.json'),
+    HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.join(libPath, 'index.js'),
+    output: {
+        path: wwwPath,
+        filename: 'prototype-[hash:6].js'
+    },
+    module: {
+        loaders: [{
+            test: /[\/]angular\.js$/,
+            loader: 'expose?angular!exports?window.angular'
+        }, {
+            test: /[\/]lodash\.js$/,
+            loader: 'expose?_'
+        }, {
+            test: /[\/]ionic\.js$/,
+            loader: 'exports?ionic' // For non commonJs
+        }, {
+            test: /\.html$/,
+            loader: 'html'
+        }, {
+            test: /\.json$/,
+            loader: "json"
+        }, {
+            test: /\.(png|jpg)$/,
+            loader: 'file?name=img/[name].[ext]'
+        }, {
+            test: /\.css$/,
+            loader: "style!css"
+        }, {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: "ng-annotate?add=true!babel"
+        }, {
+            test: /\.scss$/,
+            loader: "style!css!autoprefixer!sass"
+        }, {
+            test: [/ionicons\.svg/, /ionicons\.eot/, /ionicons\.ttf/, /ionicons\.woff/],
+            loader: 'file?name=fonts/[name].[ext]'
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.json', '.scss', '.html'],
+        root: [
+            libPath,
+            path.join(__dirname, 'node_modules')
+        ],
+        moduleDirectories: [
+            'node_modules'
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            pkg: pkg,
+            template: path.join(libPath, 'index.html')
+        })
+    ]
+};
